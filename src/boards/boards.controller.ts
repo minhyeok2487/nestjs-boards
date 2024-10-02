a628d9ae1ff99a982af9120ded383b1fdfb5fec5
Minhyeok1995 <repeater2487@naver.com>
2024-10-02 14:35:43 +0900

    Board default CRUD + Validation

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './boards.model';
+import { CreateBoardDto } from './\bdto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoards(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Patch('/:id')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  ): Board {
+    return this.boardsService.updateBoardStatus(id, status);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
 }
---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,22 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../boards.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions: BoardStatus[] = [
+    BoardStatus.PUBLIC,
+    BoardStatus.PRIVATE,
+  ];
+
+  transform(value: string): BoardStatus {
+    const upperValue = value.toUpperCase() as BoardStatus;
+
+    if (!this.isStatusValid(upperValue)) {
+      throw new BadRequestException(`${value} isn't a valid status`);
+    }
+    return upperValue;
+  }
+
+  private isStatusValid(value: BoardStatus): boolean {
+    return this.StatusOptions.includes(value);
+  }
+}
